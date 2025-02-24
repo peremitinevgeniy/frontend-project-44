bb8b6929d217ec8b4af6a223c83f7841446a2905
peremitinevgeniy <peremitinevgeniy09@gmail.com>
2025-02-24 17:21:05 +0700

    Add files via upload
    peremitinevgeniy

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import { greetingMessage } from '../src/index.js'
 console.log('Welcome to the Brain Games!')
+
+greetingMessage()
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetingMessage = () => {
+    const name = readlineSync.question('May I have your name ')
+
+    console.log(`Hi, ${name}`);
+};
+
+export { greetingMessage };
