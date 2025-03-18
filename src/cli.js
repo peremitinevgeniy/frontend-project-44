89a35ffeffec90a8346651285760645d01f8bead
peremitinevgeniy <peremitinevgeniy09@gmail.com>
2025-03-18 14:25:59 +0700

    Add files via upload
    comit

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const game = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('Please enter your name: ');
+  const name = readlineSync.question('May I have your name ');
   console.log(`Hello, ${name}!`);
   return name;
 };
